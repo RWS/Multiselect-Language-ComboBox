45d9f5c17c5d08b7fd2da80ddd46c609fd075afc
Sorin Dolha <sdolha@sdl.com>
2023-09-12 09:38:52 +0300

    Baseline for implementation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Multiselect-Language-ComboBox
+
+## Overview
+The multi selection language combo box is a WPF custom control with multiple language-selection capabilities, along with customizable features to group, sort and filter items in the collection.
---
+++ Rws.MultiselectLanguageComboBox/Rws.MultiselectLanguageComboBox/MultiselectLanguageComboBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Rws.MultiselectLanguageComboBox
+{
+    public class MultiselectLanguageComboBox : Control
+    {
+        static MultiselectLanguageComboBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MultiselectLanguageComboBox), new FrameworkPropertyMetadata(typeof(MultiselectLanguageComboBox)));
+        }
+    }
+}
